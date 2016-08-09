b859954a397f8a089efa85e44928d1bfecffa06d
Watrelos Victor <victor.watrelos@gmail.com>
2016-08-09 22:43:46 +0200

    Fix colleen

--- Colleen.c
+++ Colleen.c
@@ -4,8 +4,8 @@
 */
 void	ft(void)
 {
-	char *str = "#include <stdio.h>%c/*%c%cFirst comment%c/*%cvoid	ft(void)%c{%c%cchar *str = %c%s%c;%c	printf(str, 10, 10, 10, 10, 9, 34, str, 34, 10, 10, 10, 10, 10, 10, 9, 10, 10, 10);%c}%cint main()%c{%c	/* second comment */%c%cft();%c	return (0);%c}%c";
-	printf(str, 10, 10, 10, 9, 10, 10, 10, 10, 9, 34, str, 34, 10, 10, 10, 10, 10, 10, 9, 10, 10, 10);
+	char *str = "#include <stdio.h>%c/*%c%cFirst comment%c*/%cvoid	ft(void)%c{%c%cchar *str = %c%s%c;%c	printf(str, 10, 10, 9, 10, 10, 10, 10, 9, 34, str, 34, 10, 10, 10, 10, 10, 10, 9, 10, 10, 9, 10, 10, 10);%c}%cint main()%c{%c/*%c%csecond comment%c*/%c%cft();%c	return (0);%c}%c";
+	printf(str, 10, 10, 9, 10, 10, 10, 10, 9, 34, str, 34, 10, 10, 10, 10, 10, 10, 9, 10, 10, 9, 10, 10, 10);
 }
 int main()
 {
